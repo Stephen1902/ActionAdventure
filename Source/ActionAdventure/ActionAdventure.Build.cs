4eaf55fc3ec98e9ccb031bd302a01ed497239912
Stephen1902 <s_nichols_other@yahoo.co.uk>
2023-03-31 18:45:15 +0100

    Initial Commit

---
+++ Source/ActionAdventure/ActionAdventure.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ActionAdventure : ModuleRules
+{
+	public ActionAdventure(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
